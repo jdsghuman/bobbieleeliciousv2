0668fe8ea07a9645dba32ab6b15b1cd73f16ff1c
JD Ghuman <jdshguman@gmail.com>
2021-02-06 13:19:57 -0600

    Add subscribe banner for desktop

--- components/Subscribe/Banner.tsx
+++ components/Subscribe/Banner.tsx
@@ -1,20 +1,28 @@
-import Button from '../Button/Button'
+import Button from "../Button/Button";
 import styles from "./Banner.module.scss";
 
 const SubscribeBanner = () => {
   return (
     <div className={styles.banner}>
       <div className={styles["banner__bg-banner"]} />
-      <div>
-        <input type="text" />
-        <p>Subscribe to receive updates!</p>
-        <Button
-        className={styles.banner__button}
-        type={'button'}
-        accent
-        >
-          Subscribe
+      <div className={styles.banner__container}>
+        <p className={styles.banner__text}>Subscribe to receive updates!</p>
+        <div className={styles.form}>
+          <input
+            id="email"
+            name="email"
+            placeholder="Email"
+            className={styles.form__field}
+            type="email"
+            required
+          />
+          <label className={styles.form__label} htmlFor="email">
+            Email
+          </label>
+          <Button className={styles.banner__button} type={"button"} accent>
+            Subscribe
           </Button>
+        </div>
       </div>
     </div>
   );
