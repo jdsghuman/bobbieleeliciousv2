406d5ce11fddd813e79d13983d0ebccd34620792
JD Ghuman <jdshguman@gmail.com>
2021-02-11 21:42:31 -0600

    Update scripts in package.json

--- components/Carousel/CarouselItem/CarouselItem.tsx
+++ components/Carousel/CarouselItem/CarouselItem.tsx
@@ -1,9 +1,5 @@
 import Link from 'next/link'
-import classNames from 'classnames/bind'
 import styles from './CarouselItem.module.scss'
-import Button from '../../Button/Button'
-
-const cx = classNames.bind(styles)
 
 const CarouselItem = ({ imageDetails }) => {
   const truncateDescription = (description) => {
