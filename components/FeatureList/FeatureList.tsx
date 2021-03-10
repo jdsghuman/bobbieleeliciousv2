b78c336ecf11728bc02a0a11ee2b5c4813d0f131
JD Ghuman <jdshguman@gmail.com>
2021-03-09 23:32:05 -0600

    Add author name and image to post display

--- components/FeatureList/FeatureList.tsx
+++ components/FeatureList/FeatureList.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import classNames from 'classnames/bind'
 import { useRouter } from 'next/router'
 import Button from '../Button/Button'
-import { truncateText } from '../Util/Util'
+import { formatDate, truncateText } from '../Util/Util'
 import styles from './FeatureList.module.scss'
+
+const cx = classNames.bind(styles)
 
 const FeatureList = ({ articles, slug, title }) => {
   const router = useRouter()
@@ -36,11 +39,41 @@ const FeatureList = ({ articles, slug, title }) => {
                 ? truncateText(article.fields.description, 154)
                 : article.fields.description}
             </p>
-            <Link href={`/${slug}/` + article.fields.slug}>
-              <Button className={styles.item__button} type="button" accent>
-                Read more
-              </Button>
-            </Link>
+            <div
+              className={cx('item__container__button', {
+                'item__container__button--center': article.sys.contentType.sys.id !== 'blogPost',
+              })}
+            >
+              <div className={styles.item__container__button__author}>
+                {article.sys.contentType.sys.id === 'blogPost' &&
+                  article?.fields?.author?.fields?.image && (
+                    <>
+                      <div className={styles.item__container__button__author__image__container}>
+                        <Image
+                          src={article?.fields?.author?.fields?.image}
+                          alt={article?.fields?.author?.fields?.name}
+                          className={styles.item__container__button__image}
+                          width={40}
+                          height={40}
+                        />
+                      </div>
+                      <div className={styles.item__container__button__author__name__container}>
+                        <p className={styles.item__container__button__author__name}>
+                          {article.fields.author.fields.name}
+                        </p>
+                        <p className={styles.item__container__button__author__date}>
+                          {formatDate(article.fields.publishDate)}
+                        </p>
+                      </div>
+                    </>
+                  )}
+              </div>
+              <Link href={`/${slug}/` + article.fields.slug}>
+                <Button className={styles.item__button} type="button" accent>
+                  Read more
+                </Button>
+              </Link>
+            </div>
           </div>
         ))}
       </div>
--- components/Util/Util.ts
+++ components/Util/Util.ts
@@ -1,3 +1,8 @@
 export const truncateText = (text, maxLength) => {
   return text && text.length > maxLength ? `${text.substring(0, maxLength)}...` : text
 }
+
+export const formatDate = (dateString) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }
+  return new Date(dateString).toLocaleDateString(undefined, options)
+}
