890dcfcdc9ded4cf0ac686d2d5de6a863820ab46
JD Ghuman <jdshguman@gmail.com>
2021-03-17 23:35:43 -0500

    Update share icons

--- components/SocialMedia/ShareIcons/ShareIconItem.tsx
+++ components/SocialMedia/ShareIcons/ShareIconItem.tsx
@@ -21,6 +21,8 @@ import {
 } from 'react-share'
 import styles from './ShareIcons.module.scss'
 
+const colorIcons = 'rgb(233, 206, 194)'
+
 const ShareIconItem = ({ postImage, postName }) => {
   const [url, setUrl] = useState('')
 
@@ -35,24 +37,24 @@ const ShareIconItem = ({ postImage, postName }) => {
         hashtag={'Bobbieleelicious'}
         className={styles.icon}
       >
-        <FacebookIcon size={'2rem'} round={true} />
+        <FacebookIcon size={'2rem'} round={true} bgStyle={{ fill: `${colorIcons}` }} />
       </FacebookShareButton>
       <PinterestShareButton url={url} media={postImage} className={styles.icon}>
-        <PinterestIcon size={'2rem'} round />
+        <PinterestIcon size={'2rem'} round bgStyle={{ fill: `${colorIcons}` }} />
       </PinterestShareButton>
       <EmailShareButton
         url={url}
         subject={'Check out this recipe on Bobbieleelicious'}
         className={styles.icon}
       >
-        <EmailIcon size={'2rem'} round />
+        <EmailIcon size={'2rem'} round bgStyle={{ fill: `${colorIcons}` }} />
       </EmailShareButton>
       <WhatsappShareButton
         url={url}
         title={'Check out this recipe on Bobbieleelicous'}
         className={styles.icon}
       >
-        <WhatsappIcon size={'2rem'} round={true} />
+        <WhatsappIcon size={'2rem'} round={true} bgStyle={{ fill: `${colorIcons}` }} />
       </WhatsappShareButton>
     </div>
   )
