ba0f174e021b14cad917fbc31aa672452e63d5fc
JD Ghuman <jdshguman@gmail.com>
2021-03-18 00:02:49 -0500

    Update date format function

--- components/Util/Util.ts
+++ components/Util/Util.ts
@@ -1,8 +1,11 @@
-export const truncateText = (text: string, maxLength: number) => {
+export const truncateText = (text, maxLength) => {
   return text && text.length > maxLength ? `${text.substring(0, maxLength)}...` : text
 }
 
 export const formatDate = (dateString) => {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' }
-  return new Date(dateString).toLocaleDateString(undefined, options)
+  return new Date(dateString).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
 }
