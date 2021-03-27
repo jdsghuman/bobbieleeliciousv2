25a1fb7b8bd6463ae3aff68d5daa601d706c84a7
JD Ghuman <jdshguman@gmail.com>
2021-03-27 00:40:09 -0500

    Add social icons on posts

--- components/BlogController/BlogDetail/BlogDescription/BlogDescription.tsx
+++ components/BlogController/BlogDetail/BlogDescription/BlogDescription.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useRef, useState } from 'react'
+import classNames from 'classnames/bind'
 import ReactMarkdown from 'react-markdown'
 import AuthorItem from '../../../Author/AuthorItem'
 import { BlogPropType } from '../../../PropTypes/PropTypes'
@@ -6,9 +8,52 @@ import ShareIconItem from '../../../SocialMedia/ShareIcons/ShareIconItem'
 import styles from './BlogDescription.module.scss'
 import Signature from '../../../Signature/Signature'
 
+const cx = classNames.bind(styles)
+
 const BlogDescription = ({ blog }: BlogPropType) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const observer = useRef<any>()
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries
+    setIsVisible(entry.isIntersecting || entry.boundingClientRect.top < 400)
+  }
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1.0,
+  }
+
+  const iconRef = useCallback(
+    async (node) => {
+      await initializeObserver()
+      if (observer.current) observer.current.disconnect()
+      observer.current = new IntersectionObserver(callbackFunction, options)
+      if (node) observer.current.observe(node)
+    },
+    [isVisible]
+  )
+
+  async function initializeObserver() {
+    if (!('IntersectionObserver' in window)) {
+      //     // This is specifically for Safari - Polyfill
+      await import('intersection-observer')
+    }
+  }
   return (
     <div className={styles.container}>
+      <div
+        className={cx('share', {
+          'share--hide': isVisible,
+        })}
+      >
+        <ShareIconItem
+          className={'container__share__side'}
+          postImage={blog.fields.image}
+          postName={blog.fields.title}
+          iconSize={'3rem'}
+        />
+      </div>
       <ReactMarkdown
         linkTarget="_blank"
         className={styles.markdown}
@@ -17,7 +62,11 @@ const BlogDescription = ({ blog }: BlogPropType) => {
       <Signature author={blog.fields.author.fields.name} />
       <div className={styles['container--blog--icons']}>
         <AuthorItem blog={blog} />
-        <ShareIconItem postImage={blog.fields.image} postName={blog.fields.title} />
+        <ShareIconItem
+          iconRef={iconRef}
+          postImage={blog.fields.image}
+          postName={blog.fields.title}
+        />
       </div>
       {blog?.fields?.tag && <PostTags tags={blog.fields.tag} />}
     </div>
--- components/SocialMedia/ShareIcons/ShareIcons.tsx
+++ components/SocialMedia/ShareIcons/ShareIcons.tsx
@@ -1,11 +1,16 @@
 import ShareIconItem from './ShareIconItem'
 import styles from './ShareIcons.module.scss'
 
-const ShareIcons = ({ postImage, postName }) => {
+interface ShareIconsProps {
+  postImage: string
+  postName: string
+  iconRef: any
+}
+const ShareIcons = ({ postImage, postName, iconRef }: ShareIconsProps) => {
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Share the love</h3>
-      <ShareIconItem postName={postName} postImage={postImage} />
+      <ShareIconItem iconRef={iconRef} postName={postName} postImage={postImage} />
     </div>
   )
 }
