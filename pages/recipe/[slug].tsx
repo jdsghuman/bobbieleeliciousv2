114e7646620ac1515deef5df42374aa8f84b0c16
JD Ghuman <jdshguman@gmail.com>
2021-04-06 17:35:19 -0500

    Update meta tags

--- pages/recipe/[slug].tsx
+++ pages/recipe/[slug].tsx
@@ -51,7 +51,7 @@ const Recipe = ({ recipe, morePosts }: RecipePropType) => {
   }
 
   const postMetaTags: MetaTags = {
-    canonical: 'https://www.bobbieleelicious.com',
+    canonical: `https://www.bobbieleelicious.com/recipe/${recipe.fields.slug}`,
     description: `${
       recipe.fields.metaDescription
         ? recipe.fields.metaDescription
