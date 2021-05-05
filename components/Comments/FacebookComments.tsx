704df5ed7b0045448ea21cfaf6f2374715e7e51e
JD Ghuman <jdshguman@gmail.com>
2021-05-05 08:32:54 -0500

    Add loading spinner

--- components/Comments/FacebookComments.tsx
+++ components/Comments/FacebookComments.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { BlogsPropType, RecipesPropType } from '../PropTypes/PropTypes'
+import Spinner from '../Spinner/Spinner'
 import styles from './FacebookComments.module.scss'
 
 interface CommentsPropType {
@@ -7,6 +8,7 @@ interface CommentsPropType {
 }
 
 const FacebookComments = ({ post }: CommentsPropType) => {
+  const [showSpinner, setShowSpinner] = useState(true)
   const postType = post.sys.contentType.sys.id === 'recipe' ? 'recipe' : 'blog'
 
   console.log('render CommentsFacebook')
@@ -17,6 +19,7 @@ const FacebookComments = ({ post }: CommentsPropType) => {
     facebookScript.async = true
     facebookScript.src = `https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v7.0&appId=${process.env.FACEBOOK_APP_ID}&autoLogAppEvents=1`
     document.body.appendChild(facebookScript)
+    setShowSpinner(false)
   }, [])
 
   useEffect(() => {
@@ -30,12 +33,16 @@ const FacebookComments = ({ post }: CommentsPropType) => {
 
   return (
     <div className={styles.container}>
+      {showSpinner && (
+        <div>
+          <Spinner />
+        </div>
+      )}
       <div id="fb-root"></div>
       <div
         className="fb-comments"
         data-href={`https://bobbieleelicious.com/${postType}/${post.fields.slug}`}
         data-numposts="10"
-        data-width={'100%'}
         data-mobile="true"
       ></div>
     </div>
