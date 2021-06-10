db7e59ab14ae9a148689edef46924dbd70e9df0c
JD Ghuman <jdshguman@gmail.com>
2021-06-09 22:28:15 -0500

    Update about me

--- data/data.ts
+++ data/data.ts
@@ -6,8 +6,8 @@ export const aboutText = `Hi, I'm Bobbie. I am a mom of two sweet little
   made breads, jams and an abundance of local farm to table produce, 
   I grew to appreciate fresh, wholesome foods. I am very passionate 
   about good food, I love creating healthy recipes that nourish our 
-  mind, body and soul. --  I really hope you enjoy our vegetarian friendly 
-  recipes, most have vegan alternatives as well as gluten free. You will 
+  mind, body and soul. --  I really hope you enjoy the healthy living blog and our plant focused 
+  recipes. Most  of the recipes have vegan alternatives and/or also gluten free options. You will 
   see a variety of different dishes including Mediterranean, 
   Indian (married into a wonderful Punjabi family), Mexican, etc... 
   Plenty of juices, smoothies, vibrant salads - you get my point, 
@@ -15,7 +15,7 @@ export const aboutText = `Hi, I'm Bobbie. I am a mom of two sweet little
   If you are looking for quick, easy, fresh, wholesome, vegetarian 
   recipes made with love to nourish your body, to nourish your cells... 
   my friend, you are in the right place! -- When it comes to ones health 
-  and wellness, eating a healthy diet is one of the greatest gifts we can 
+  and wellness, eating a healthy diet and making healthy lifestyle choices is one of the greatest gifts we can 
   give ourselves and families. Life can be complicated, eating a well 
   balanced, plant focused diet shouldn't be!`
 
